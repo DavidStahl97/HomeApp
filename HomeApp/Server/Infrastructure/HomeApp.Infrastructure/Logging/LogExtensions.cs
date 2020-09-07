5191d55e9b1f0c0e1846ec2d42994bd26bdf06ca
de18702 <stahl.d@loh-services.de>
2020-09-07 17:24:56 +0200

    Logging: store class, method, line

---
+++ HomeApp/Server/Infrastructure/HomeApp.Infrastructure/Logging/LogExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HomeApp.Infrastructure.Logging
+{
+    static class LogExtensions
+    {
+        public static ILogger AddCodeInformations(this ILogger logger, string path, string method, int line)
+        {
+            var classString = path.Split('\\').LastOrDefault();
+            return logger
+                .ForContext("class", classString)
+                .ForContext("method", method)
+                .ForContext("line", line);
+        }
+    }
+}
