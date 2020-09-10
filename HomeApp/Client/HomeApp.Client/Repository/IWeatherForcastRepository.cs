71ad5fa45d404cb966b43247ad90716c66dd21bb
de18702 <stahl.d@loh-services.de>
2020-09-10 15:28:11 +0200

    add old classes: HttpService and WeatherForecastRepository

---
+++ HomeApp/Client/HomeApp.Client/Repository/IWeatherForcastRepository.cs
@@ -0,0 +1,13 @@
+﻿using HomeApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeApp.Client.Repository
+{
+    public interface IWeatherForcastRepository
+    {
+        Task<IEnumerable<WeatherForecast>> Get();
+    }
+}
