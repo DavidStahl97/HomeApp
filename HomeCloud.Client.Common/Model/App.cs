e30c10be543bbc32236023e4c02107668f7f4397
DavidStahl97 <david.stahl97@outlook.com>
2020-09-18 13:11:35 +0200

    fix navigation to apps

--- HomeCloud.Client.Common/Model/App.cs
+++ HomeCloud.Client.Common/Model/App.cs
@@ -11,8 +11,15 @@ namespace HomeCloud.Client.Common.Model
 
         public string Description { get; set; }
 
+        public string AppLink { get; set; }
+
         public IEnumerable<NavigationItem> NavigationItems { get; set; }
 
         public NavigationItem FirstNavigationItem => NavigationItems.First();
+
+        public string NavigationLink(NavigationItem navItem)
+        {
+            return $"/app/{ AppLink }/{ navItem.Link }";
+        }
     }
 }
