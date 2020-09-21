3ba782ac48ebc42aed5361c0314ad92383461c43
DavidStahl97 <david.stahl97@outlook.com>
2020-09-21 14:10:29 +0200

    HomeCloud.Users.Service: ensure migration

---
+++ HomeCloud.Users/HomeCloud.Users.Service/EnsureMigrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeCloud.Users.Service
+{
+    public static class EnsureMigrationExtensions
+    {
+        public static IHost EnsureMigration<T>(this IHost host)
+            where T : DbContext
+        {
+            using (var scrope = host.Services.CreateScope())
+            {
+                var context = scrope.ServiceProvider.GetService<T>();
+                context.Database.Migrate();
+            }
+
+            return host;
+        }
+    }
+}
