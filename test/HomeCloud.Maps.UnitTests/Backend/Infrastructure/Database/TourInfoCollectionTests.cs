23b36068d2e0958936d75475762c5a5ce096d999
DavidStahl97 <david.stahl97@outlook.com>
2021-02-21 14:42:55 +0100

    FindPageAsync UnitTests

--- test/HomeCloud.Maps.UnitTests/Backend/Infrastructure/Database/TourInfoCollectionTests.cs
+++ test/HomeCloud.Maps.UnitTests/Backend/Infrastructure/Database/TourInfoCollectionTests.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using static OneOf.Types.TrueFalseOrNull;
 
 namespace HomeCloud.Maps.UnitTests.Backend.Infrastructure.Database
 {
@@ -35,7 +36,7 @@ namespace HomeCloud.Maps.UnitTests.Backend.Infrastructure.Database
         [InlineData("Feldberg", "eld")]
         [InlineData("Feldberg - Bad Homburg", "- Bad ho")]
         [InlineData("Feldberg - Bad Homburg", "Feldberg - Bad Homburg")]
-        public async Task TourInfo_SearchWithFilter_ShouldFindTour(string tourName, string filter)
+        public async Task FindPageAsync_SearchWithFilter_ShouldFindTour(string tourName, string filter)
         {
             // Arrange
             var data = Fixture.CreateList<TourInfo>(10);
@@ -53,6 +54,108 @@ namespace HomeCloud.Maps.UnitTests.Backend.Infrastructure.Database
 
             var actual = Page.First();
             actual.Should().BeEquivalentTo(expected, 
+                options => options.AddDateTimeCloseToExpected());
+        }
+
+        [Fact]
+        public async Task FindPageAsync_SearchWithoutFilter()
+        {
+            // Arrange
+            const string userId = "userid-test";
+            
+            var data = Fixture.CreateList<TourInfo>(10);
+            
+            var expected = new[]
+            {
+                data.First(), data.Skip(5).First(), data.Last()
+            }.ToList();
+            expected.ForEach(x => x.UserId = userId);
+
+            await Repository.TourInfoCollection.InsertManyAsync(expected);
+
+            // Act
+            var (actual, count) = await Repository.TourInfoCollection
+                .FindPageAsync(userId, 0, 10, new Null());
+
+            // Assert
+            count.Should().Be(3);
+            actual.Should().BeEquivalentTo(expected,
+                options => options.AddDateTimeCloseToExpected());
+        }
+
+        [Theory]
+        [InlineData("Hausberg", "berg")]
+        [InlineData("Feldberg - Bad Homburg", "Feld")]
+        [InlineData("Feldberg - Bad", "feld")]
+        [InlineData("Feldberg", "eld")]
+        [InlineData("Feldberg - Bad Homburg", "- Bad ho")]
+        [InlineData("Feldberg - Bad Homburg", "Feldberg - Bad Homburg")]
+        public async Task FindPageAsync_ShouldReturnTotalCount(string tourName, string filter)
+        {
+            // Arrange
+            const string userId = "userId-test";
+            const int pageSize = 10;
+
+            var data = Fixture.CreateList<TourInfo>(61).ToList();
+            
+            var fromUser = data.Take(50).ToList();
+            fromUser.ForEach(x => x.UserId = userId);
+
+            var expected = fromUser.Take(29).ToList();
+            expected.ForEach(x => x.Name += tourName);
+
+            await Repository.TourInfoCollection.InsertManyAsync(data);
+
+            // Act
+            var (page, count) = await Repository.TourInfoCollection.FindPageAsync(userId, 0, pageSize, filter);
+
+            // Assert
+            count.Should().Be(29);
+        }
+
+        [Theory]
+        [InlineData("Hausberg", "berg")]
+        [InlineData("Feldberg - Bad Homburg", "Feld")]
+        [InlineData("Feldberg - Bad", "feld")]
+        [InlineData("Feldberg", "eld")]
+        [InlineData("Feldberg - Bad Homburg", "- Bad ho")]
+        [InlineData("Feldberg - Bad Homburg", "Feldberg - Bad Homburg")]
+        public async Task FindPageAsync_ShouldReturnCorrectPagination(string tourName, string filter)
+        {
+            // Arrange
+            const string userId = "userId-test";
+            const int pageSize = 10;
+
+            var data = Fixture.CreateList<TourInfo>(61).ToList();
+
+            var fromUser = data.Take(50).ToList();
+            fromUser.ForEach(x => x.UserId = userId);
+
+            var expected = fromUser.Take(29).ToList();
+            expected.ForEach(x => x.Name += tourName);
+
+            var expectedPageOne = expected.Take(10).ToList();
+            var expectedPageTwo = expected.Skip(10).Take(10).ToList();
+            var expectedPageThree = expected.Skip(20).ToList();
+
+            await Repository.TourInfoCollection.InsertManyAsync(data);
+
+            // Act
+            var (actualPageOne, _) = await Repository.TourInfoCollection.FindPageAsync(userId, 0, pageSize, filter);
+            var (actualPageTwo, _) = await Repository.TourInfoCollection.FindPageAsync(userId, 1, pageSize, filter);
+            var (actualPageThree, _) = await Repository.TourInfoCollection.FindPageAsync(userId, 2, pageSize, filter);
+
+            // Assert
+            actualPageOne.Should().HaveCount(10);
+            actualPageOne.Should().BeEquivalentTo(expectedPageOne,
+                options => options.AddDateTimeCloseToExpected());
+
+            actualPageTwo.Should().HaveCount(10);
+            actualPageTwo.Should().BeEquivalentTo(expectedPageTwo,
+                options => options.AddDateTimeCloseToExpected());
+
+            actualPageThree.Should().HaveCount(9);
+            actualPageThree.Should().BeEquivalentTo(expectedPageThree,
                 options => options.AddDateTimeCloseToExpected());
         }
     }
