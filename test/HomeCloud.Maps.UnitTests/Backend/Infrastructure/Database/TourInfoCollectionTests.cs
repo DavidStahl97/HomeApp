a25c5ee14b209e9f52ab24431e015d304c8dfdf0
DavidStahl97 <david.stahl97@outlook.com>
2021-02-21 13:41:20 +0100

    TourInfo_SearchWithFilter_ShouldFindTour

--- test/HomeCloud.Maps.UnitTests/Backend/Infrastructure/Database/TourInfoCollectionTests.cs
+++ test/HomeCloud.Maps.UnitTests/Backend/Infrastructure/Database/TourInfoCollectionTests.cs
@@ -27,5 +27,33 @@ namespace HomeCloud.Maps.UnitTests.Backend.Infrastructure.Database
         public Task TourInfo_FirstAsync()
             => FirstAsync(expected =>
                 Repository.TourInfoCollection.FirstAsync(expected.UserId, expected.TourId));
+
+        [Theory]
+        [InlineData("Hausberg", "berg")]
+        [InlineData("Feldberg - Bad Homburg", "Feld")]
+        [InlineData("Feldberg - Bad", "feld")]
+        [InlineData("Feldberg", "eld")]
+        [InlineData("Feldberg - Bad Homburg", "- Bad ho")]
+        [InlineData("Feldberg - Bad Homburg", "Feldberg - Bad Homburg")]
+        public async Task TourInfo_SearchWithFilter_ShouldFindTour(string tourName, string filter)
+        {
+            // Arrange
+            var data = Fixture.CreateList<TourInfo>(10);
+            var expected = data.Skip(5).First();
+            expected.Name = tourName;
+
+            await Repository.TourInfoCollection.InsertManyAsync(data);
+
+            // Act
+            var (Page, Count) = await Repository.TourInfoCollection
+                .FindPageAsync(expected.UserId, 0, 10, filter);
+
+            // Assert
+            Page.Should().HaveCount(1);
+
+            var actual = Page.First();
+            actual.Should().BeEquivalentTo(expected, 
+                options => options.AddDateTimeCloseToExpected());
+        }
     }
 }
